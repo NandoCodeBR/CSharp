01664c4f367a1794ba85de53bc07d495b2d83a7d
Fernando <52359881+NandoCodeBR@users.noreply.github.com>
2020-12-03 17:38:59 -0300

    Add files via upload

---
+++ Valor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double valorInvestido = 1000.0;
+            int i = 1;
+            while (i <= 12)
+            {
+                valorInvestido = valorInvestido * 1.01;
+                i += 1;
+            }
+            lbl1.Text = "Valor investido agora é " + valorInvestido;
+        
+        }
+    }
+}
