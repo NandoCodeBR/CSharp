ba66870db027dbbac02e712573ac613aae985a29
Fernando <52359881+NandoCodeBR@users.noreply.github.com>
2020-12-03 17:36:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+namespace exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 2;
+            for (int i = 0; i < 5; i += 1)
+            {
+                total = total * 2;
+            }
+            Console.Write("O	total	é:	" + total);
+            
+        }
+    }
+}
