96be028617ae8cb4553abd273985256f05152fe8
Fernando <fernando.muniz@outlook.com.br>
2020-12-17 16:27:27 -0300

    constantes

---
+++ Constantes/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string nome = "Fernando";
+            const double pi = 3.1415;
+
+            //nome="Rodrigo";
+            //pi = 3.4;         Ambas as linhas geram erro, pois a constante não pode ter seu valor alterado
+
+            Console.Write("O seu nome é: {0}\nPi: {1}",nome,pi);
+        }
+    }
+}
